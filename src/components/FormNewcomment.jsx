3ddb3184e1122f75e3a8163e3e07328f45470e20
thiagomag94 <thiagomag94@gmail.com>
2022-11-19 08:59:28 -0300

    adding feature delete comment

--- src/components/FormNewcomment.jsx
+++ src/components/FormNewcomment.jsx
@@ -3,9 +3,9 @@ const FormNewcomment = (event) =>{
     
     return (
         
-        <div onSubmit = {event.onSubmit} className="w-11/12 h-fit bg-neutral-800 rounded-lg mb-16 ">
-            <form className="text-primary-color flex flex-col px-6 shadow-lg gap-6 py-4 ">
-                <h1 className="w-fit py-2 border-b-2 ">Add new comment</h1>
+        <div onSubmit = {event.onSubmit} className="w-11/12 h-fit bg-neutral-800 rounded-lg mb-16  ">
+            <form className="text-primary-color flex flex-col px-6 shadow-lg gap-6 py-4">
+                <h1 className="w-fit text-md py-2 border-b-2 ">Add new comment</h1>
                 <input type="text"  onChange = {event.onChangeName} placeholder="Type your name here.." className="rounded-sm p-2" required></input>
                 <input type="email" onChange = {event.onChangeEmail} placeholder="Type your best e-mail here.." className="rounded-sm p-2" required></input>
                 <textarea type="textarea" onChange = {event.onChangeMessage} rows='4' placeholder="Type your comment here.." className="rounded-sm px-2" required></textarea>
