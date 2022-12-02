8c47968ee3c153765764f9873939da6830b95e4f
thiagomag94 <thiagomag94@gmail.com>
2022-12-02 06:25:07 -0300

    adjusting padding to look better in small screens

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -9,7 +9,7 @@ const Comment = (props) => {
     
     return (
         
-        <div id="div-comment"  className="w-11/12 mb-12 text-sm flex flex-col justify-center items-center bg-neutral-800 rounded-lg drop-shadow-sm gap-4 relative py-4 pb-6 px-4 flex-wrap relative lg:w-2/5 ">
+        <div id="div-comment"  className="w-full mb-12 text-sm flex flex-col justify-center items-center bg-neutral-800 rounded-lg drop-shadow-sm gap-4 relative py-4 pb-6 px-4 flex-wrap relative md:w-11/12 lg:w-2/5 ">
             <p className="text-primary-color  py-2">{name}<span className="text-slate-200 font-extralight text-sm "> - {email}</span></p>
             <div className="w-4/5 mb-10 text-slate-400 font-extralight p-10 rounded-lg"><p align="justify">{message}</p></div>    
             <p className="text-slate-400 font-extralight absolute right-4 bottom-2 opacity-40"> {date} às <span>{hour}h:{minutes}</span></p>
--- src/components/FormNewcomment/FormNewcomment.jsx
+++ src/components/FormNewcomment/FormNewcomment.jsx
@@ -9,7 +9,7 @@ const FormNewcomment = (event) =>{
     return (
         
         <div onSubmit = {event.onSubmit} className="w-11/12 h-fit bg-neutral-800 rounded-lg mb-16 lg:w-2/5  ">
-            <form className=" flex flex-col px-16 drop-shadow-xl gap-6 py-4">
+            <form className=" flex flex-col px-8 drop-shadow-xl gap-6 py-4 lg:px-16">
                 <h1 className="w-fit text-md text-primary-color py-2 border-b-2  ">Add new comment</h1>
                 <input id="text" type="text"  onChange = {event.onChangeName} placeholder="Type your name here.." className="text-slate-200 bg-transparent border border-neutral-700 rounded-sm p-2 focus:outline-none focus:ring-2 focus:ring-primary-color" required></input>
                 <input id="email" type="email" onChange = {event.onChangeEmail} placeholder="Type your best e-mail here.." className="text-slate-200 bg-transparent border border-neutral-700 rounded-sm p-2  focus:outline-none focus:ring-2 focus:ring-primary-color invalid:border-pink-500 invalid:text-pink-500 focus:invalid:border-pink-500 focus:invalid:ring-pink-500" required></input>
